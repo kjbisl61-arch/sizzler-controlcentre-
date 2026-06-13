aa7ec6d353d470f4aebd3a789c3d4946d3e906a6
kjbisl61-arch <kjbisl61@gmail.com>
2026-06-13 12:06:27 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+mport React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../ControlCentre.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
